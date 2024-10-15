397871885d271c91a2679b0d5d6bb6d170b42940
guweisheng <guweisheng@genew.com>
2024-10-15 18:00:38 +0800

    1. 增加：gitlab-ci 流水 2. 增加：推送到网盘

--- tar_package.sh
+++ tar_package.sh
@@ -11,7 +11,7 @@ TAR_SCRIPT_PATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && 
 NOW="$(date +%Y_%m%d_%H%M%S)"
 
 # 删除生成目录
-/bin/rm -rf "${TAR_SCRIPT_PATH}"/distr/
+/usr/bin/rm -rf "${TAR_SCRIPT_PATH}"/distr/
 
 # 创建目录
 mkdir -p "${TAR_SCRIPT_PATH}"/distr/
@@ -20,12 +20,14 @@ mkdir -p "${TAR_SCRIPT_PATH}"/distr/
 /bin/rm -rf "${TAR_SCRIPT_PATH}"/{logs,backup,log}
 
 # 排除assembly目录,并生成tar安装包
-tar --exclude="./*/tar_package.sh" \
-    --exclude="./*/distr" \
-    --exclude="./*/.git" \
-    --exclude="./*/.gitignore" \
-    --exclude="./*/*/.gitignore" \
-    --exclude="./*/publish_file_server.sh" \
+tar --exclude="**/tar_package.sh" \
+    --exclude="**/distr" \
+    --exclude="**/.git" \
+    --exclude="**/.gitkeep" \
+    --exclude="**/.gitignore" \
+    --exclude="**/publish_file_server.sh" \
+    --exclude="**/.gitlab-ci.yml" \
+    --exclude="**/push_storage.sh" \
     -cvzf "${TAR_SCRIPT_PATH}"/distr/auto_add_firewall_rules-x86_64-"${NOW}".tar.gz \
     -C "${TAR_SCRIPT_PATH}"/../ \
     ./auto_add_firewall_rules/
